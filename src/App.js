e29acecb8c8a30e002a6e97458db8a5938a84906
Kareem Fakhuri <kareemfakhuri@gmail.com>
2023-01-21 16:14:51 -0500

    sample chart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { ResponsivePie } from '@nivo/pie'
 
-function App() {
+const data = [
+  {
+    id: "java",
+    label: "java",
+    value: 195,
+    color: "hsl(90, 70%, 50%)"
+  },
+  {
+    id: "erlang",
+    label: "erlang",
+    value: 419,
+    color: "hsl(56, 70%, 50%)"
+  },
+  {
+    id: "ruby",
+    label: "ruby",
+    value: 407,
+    color: "hsl(103, 70%, 50%)"
+  },
+  {
+    id: "haskell",
+    label: "haskell",
+    value: 474,
+    color: "hsl(186, 70%, 50%)"
+  },
+  {
+    id: "go",
+    label: "go",
+    value: 71,
+    color: "hsl(104, 70%, 50%)"
+  }
+];
+
+const Pie = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ResponsivePie
+      data={data}
+      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+      innerRadius={0.5}
+      padAngle={0.7}
+      cornerRadius={3}
+      activeOuterRadiusOffset={8}
+      borderWidth={1}
+      borderColor={{ from: "color", modifiers: [["darker", 0.2]] }}
+      arcLinkLabelsSkipAngle={10}
+      arcLinkLabelsTextColor="#ffffff"
+      arcLinkLabelsThickness={2}
+      arcLinkLabelsColor={{ from: "color" }}
+      arcLabelsSkipAngle={10}
+      arcLabelsTextColor={{ from: "color", modifiers: [["darker", 2]] }}
+    />
   );
+};
+
+function App () {
+  return (
+    <div className='App'>
+      <header>
+      </header>
+      <body>
+      <div style={{height: 500}}>
+        <Pie />
+      </div>
+      </body>
+    </div>
+  )
 }
 
-export default App;
+export default App
