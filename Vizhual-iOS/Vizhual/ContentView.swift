451fe735dfa60c928c746253e7500b3c391b9d6b
Alireza <mr.toghyiani@icloud.com>
2023-01-21 20:32:14 -0500

    Make iOS Charts

---
+++ Vizhual-iOS/Vizhual/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vizhual
+//
+//  Created by Alireza Toghiani on 1/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        MyPieChart()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vizhual-iOS/Vizhual/VizhualApp.swift
@@ -0,0 +1,17 @@
+//
+//  VizhualApp.swift
+//  Vizhual
+//
+//  Created by Alireza Toghiani on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct VizhualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
